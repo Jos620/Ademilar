dd3a5af3a36ea7a10a832e94410abb074ec4b979
Mateus <mateusito10@gmail.com>
2021-03-24 23:14:23 -0300

    🌎 Global

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,14 +36,7 @@
 </template>
 
 <script>
-import Navbar from "~/components/Navbar";
-import Footer from "~/components/Footer";
-
 export default {
-    components: {
-        Navbar,
-        Footer
-    },
     methods: {
         scroll() {
             this.$refs.navbar.handleScroll();
