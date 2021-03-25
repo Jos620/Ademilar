d02a7dd044bc44d8ef502c7f60aced4ab3aefb90
Mateus <mateusito10@gmail.com>
2021-03-25 00:49:16 -0300

    🛠 Adjusts

--- components/contato/MediaButton.vue
+++ components/contato/MediaButton.vue
@@ -1,58 +1,65 @@
 <template>
-	<a
-		:href="media.link"
-		:class="
-			`h-16 w-40 rounded-xl inline-flex justify-center items-center align-middle ${media.name}`
-		"
-	>
-		<img
-			:src="icon"
-			:width="media.name == 'facebook' ? 36 : media.name == 'instagram' ? 26 : 30"
-			class="mr-4"
-		/>
-		<slot></slot>
-	</a>
+    <a
+        :href="media.link"
+        target="_blank"
+        :class="
+            `h-16 w-40 rounded-xl inline-flex justify-center items-center align-middle ${media.name}`
+        "
+    >
+        <img
+            :src="icon"
+            :width="
+                media.name == 'facebook'
+                    ? 36
+                    : media.name == 'instagram'
+                    ? 26
+                    : 30
+            "
+            class="mr-4"
+        />
+        <slot></slot>
+    </a>
 </template>
 
 <script>
 export default {
-	name: 'MediaButton',
-	props: {
-		media: {
-			type: Object,
-			required: true,
-		},
-	},
-	data() {
-		return {
-			icon: require(`@/assets/svg/social/${this.media.name}.svg`),
-		};
-	},
+    name: "MediaButton",
+    props: {
+        media: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            icon: require(`@/assets/svg/social/${this.media.name}.svg`)
+        };
+    }
 };
 </script>
 
 <style lang="postcss" scoped>
 * {
-	color: white;
-	transition: 0.2s;
+    color: white;
+    transition: 0.2s;
 }
 
 img {
-	display: inline;
+    display: inline;
 }
 
 .whatsapp {
-	@apply bg-green-400;
-	@apply hover:bg-green-500;
+    @apply bg-green-400;
+    @apply hover:bg-green-500;
 }
 
 .facebook {
-	@apply bg-blue-400;
-	@apply hover:bg-blue-500;
+    @apply bg-blue-400;
+    @apply hover:bg-blue-500;
 }
 
 .instagram {
-	@apply bg-gradient-to-br from-pink-400 to-purple-400;
-	@apply hover:from-pink-500 hover:to-purple-500;
+    @apply bg-gradient-to-br from-pink-400 to-purple-400;
+    @apply hover:from-pink-500 hover:to-purple-500;
 }
 </style>
