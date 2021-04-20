db393958b113d25307392be15468d4fe28c44edc
Mateus Ito Silva <mateusito10@gmail.com>
2021-04-20 18:20:21 -0300

    💲 Price Table

--- components/inicio/PriceTable.vue
+++ components/inicio/PriceTable.vue
@@ -4,10 +4,10 @@
 			<h1 class="price-table__title">Tabela de Créditos</h1>
 		</div>
         <div>
-            <div class="grid grid-cols-4 gap-12 place-items-center">
+            <div class="grid lg:grid-cols-4 gap-12 place-items-center">
                 <div class="mt-12">
                     <!-- TODO: terminar os filtros -->
-                    <div class="grid gap-8 grid-cols-1 place-items-center">
+                    <div class="flex lg:grid gap-8 grid-cols-1 place-items-center">
                         <img @click="filterTable('motorcycle')" src="~/assets/svg/tabela/motorcycle.svg" alt="Motos" class="sort">
                         <img @click="filterTable('car')" src="~/assets/svg/tabela/car.svg" alt="Motos" class="sort">
                         <img @click="filterTable('truck')" src="~/assets/svg/tabela/truck.svg" alt="Motos" class="sort">
@@ -15,10 +15,10 @@
                     </div>
                 </div>
                 <el-table class="col-span-3" :data="activeTable" height="500" stripe>
-                    <el-table-column prop="valor" label="Crédito" width="auto"></el-table-column>
-                    <el-table-column prop="parcela" label="Parcela" width="auto"></el-table-column>
-                    <el-table-column prop="promo" label="Promocional" width="auto"></el-table-column>
-                    <el-table-column prop="meses" label="Meses" width="130"></el-table-column>
+                    <el-table-column prop="valor" label="Crédito" width="150"></el-table-column>
+                    <el-table-column prop="parcela" label="Parcela" width="130"></el-table-column>
+                    <el-table-column prop="promo" label="Promocional" width="200"></el-table-column>
+                    <el-table-column prop="meses" label="Meses" width="120"></el-table-column>
                     <el-table-column width="150">
                         <template>
                             <a href="https://api.whatsapp.com/send?phone=554196006993&text=Oi%2C%20gostaria%20de%20simular%20um%20consórcio!" target="_blank">
